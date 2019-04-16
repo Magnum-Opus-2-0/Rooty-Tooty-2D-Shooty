a7b4912630a8f31cc6ab44862c399b36d93ecb31
Magnanimous Opicus <mbrad012@ucr.edu>
2019-04-16 12:25:54 +0000

    Implemented Health Bar UI and Explosions, made demo of tank exploding due to residual poison damage

---
+++ Assets/Scripts/Player/ExplodeBehavior.cs
@@ -0,0 +1,169 @@
+﻿using System;   // needed for Random
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeBehavior : MonoBehaviour {
+
+    public bool doExplosionDemo;
+
+    public const int NUM_OF_PIECES = 3;
+    private bool exploded;  // used to track state
+
+    // Variables and references which are configured via Unity Editor
+    public float forceOfExplosion;
+    public float radiusOfExplosion;
+
+    /// <summary>
+    /// Pieces of the tank (or whatever!) which need to explode apart.
+    /// </summary>
+    public GameObject[] pieces = new GameObject[NUM_OF_PIECES];
+
+    /// <summary>
+    /// The positions that each piece should be restored to.
+    /// This can be either the tank's last unexploded position,
+    /// or its spawn point.
+    /// If you need to modify where the tank should be restored to,
+    /// modify where you call RecordOriginalTransforms().
+    /// </summary>
+    private Vector3[] originalPositions = new Vector3[NUM_OF_PIECES];
+
+    /// <summary>
+    /// The rotations that each piece should be restored to.
+    /// Modify these the same way you would handle originalPositions.
+    /// </summary>
+    private Quaternion[] originalRotations = new Quaternion[NUM_OF_PIECES];
+
+    /// <summary>
+    /// Temporary Rigidbodies, required by the pieces for physics calculations.
+    /// These actually cannot coexist with functional tanks,
+    /// so they must be added and deleted when the tank explodes and is restored.
+    /// Before an explosion, all elements in this array are null.
+    /// </summary>
+    private Rigidbody[] rigidbodies = new Rigidbody[NUM_OF_PIECES];
+
+    /// <summary>
+    /// Explodes the pieces so they all fall apart.
+    /// This is done by assigning them each a Rigidbody at runtime,
+    /// and then applying a pseudorandom force to each of them.
+    /// </summary>
+    public void Explode() {
+
+        if (exploded) return;
+
+        System.Random r = new System.Random();
+
+        Vector3 evenExplosionOffset = new Vector3(
+            MapToRange((float)r.NextDouble(), 0f, 1f, -2f, 2f),    // x
+            -0.5f,                                                 // y
+            MapToRange((float)r.NextDouble(), 0f, 1f, -2f, 2f)     // z
+            );
+
+        Vector3 vertExplosionOffset = new Vector3(
+            MapToRange((float)r.NextDouble(), 0f, 1f, -0.7f, 0.7f),    // x
+            -1f,                                                       // y
+            MapToRange((float)r.NextDouble(), 0f, 1f, -0.7f, 0.7f)     // z
+            );
+
+        for (int i = 0; i < NUM_OF_PIECES; i++) {
+
+            // First, add the temporary Rigidbody
+            rigidbodies[i] = pieces[i].AddComponent<Rigidbody>();
+
+            // Then, apply explosion forces
+            if (i % 2 == 0) {
+                rigidbodies[i].AddExplosionForce(forceOfExplosion,
+                    pieces[i].transform.position + evenExplosionOffset, // Offset each explosion slightly for effect
+                    radiusOfExplosion, 0.0f, ForceMode.Impulse);
+            } else {
+                rigidbodies[i].AddExplosionForce(forceOfExplosion,
+                    pieces[i].transform.position + vertExplosionOffset, // Make some pieces spring up higher
+                    radiusOfExplosion, 0.0f, ForceMode.Impulse);
+            }
+
+
+        }
+
+        exploded = true;
+    }
+
+    /// <summary>
+    /// Fixes an exploded bunch of pieces.
+    /// The Rigidbodies are removed to restore functionality,
+    /// and then the pieces' transforms are all reset to their values before their explosion.
+    /// </summary>
+    public void Restore() {
+
+        if (!exploded) return;
+
+        // First, remove rigidbodies,
+        // which removes the effects of physics as well
+        for (int i = 0; i < NUM_OF_PIECES; i++) {
+
+            rigidbodies[i].transform.rotation = originalRotations[i];
+            rigidbodies[i].transform.position = originalPositions[i];
+
+            // Remove rigidbodies,
+            // which removes the effects of physics as well
+            Destroy(rigidbodies[i]);
+
+            // Then, restore original transforms
+            pieces[i].transform.rotation = originalRotations[i];
+            pieces[i].transform.position = originalPositions[i];
+            // Transforms also have a scale, but these are unchanged
+        }
+
+        exploded = false;
+    }
+
+
+    /// <summary>
+    /// Records the transforms for the individual pieces.
+    /// These will be later used to restore the pieces once they explode.
+    /// </summary>
+    private void RecordOriginalTransforms() {
+
+        for (int i = 0; i < NUM_OF_PIECES; i++) {
+
+            originalPositions[i] = pieces[i].transform.position;
+            originalRotations[i] = pieces[i].transform.rotation;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        exploded = false;
+
+        RecordOriginalTransforms();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (doExplosionDemo) {
+
+            if (Input.GetKeyDown(KeyCode.Mouse0) && !exploded) {
+
+                Debug.Log("Exploding!");
+                Explode();
+
+            } else if (Input.GetKeyDown(KeyCode.Mouse1) && exploded) {
+
+                Debug.Log("Fixing!");
+                Restore();
+            }
+        }
+
+    }
+
+    private float MapToRange(float x, float old_lo, float old_hi, float new_lo, float new_hi) {
+
+        x -= old_lo;
+        x /= old_hi - old_lo;
+
+        x *= (new_hi - new_lo);
+        x += new_lo;
+
+        return x;
+    }
+}
---
+++ Assets/Scripts/Player/HealthBehavior.cs
@@ -0,0 +1,169 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBehavior : MonoBehaviour
+{
+    public SimpleHealthBar healthBar;
+    public ExplodeBehavior explodeScript;
+
+    public const int MAX_HEALTH = 50;
+    private int currentHealth;
+
+    // Variables for demo poison function
+    public bool doPoisonDemo;   // this is initialized via the Unity editor
+    private const float POISON_PERIOD = 0.25f; // use this value only for testing
+    private const int POISON_DAMAGE = 2;
+    private static float currentTimeStep;
+
+
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = MAX_HEALTH;
+        currentTimeStep = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // This is a demo function;
+        // replace with an actual damage calculation in real usage
+        if (doPoisonDemo) update_PoisonDamage();
+    }
+
+    /// <summary>
+    /// Test function for taking damage.
+    /// The runner of this script takes damage every so often,
+    /// specified by POISON_PERIOD.
+    /// Once the player's health fully depletes,
+    /// they are marked as dead (i.e. isAlive == false).
+    /// </summary>
+    private void update_PoisonDamage() {
+
+        currentTimeStep += Time.deltaTime;
+
+        if (currentTimeStep >= POISON_PERIOD) {
+
+            bool wasAlive = isNotDead();
+
+            adjustHealth(-POISON_DAMAGE);
+
+            // Explode if you just died
+            if (wasAlive && !isNotDead())
+                explodeScript.Explode();
+
+            // Reset current time step
+            currentTimeStep = 0;
+        }
+
+        // Press space to reset
+        if (Input.GetKeyDown(KeyCode.Space)) {
+
+            Start();
+            explodeScript.Restore();
+        }
+    }
+
+    /// <summary>
+    /// Updates health bar with current health,
+    /// and sets color accordingly.
+    /// </summary>
+    public void updateHealthBar() {
+
+        // Update the health bar amount
+        healthBar.UpdateBar(currentHealth, MAX_HEALTH);
+
+        // Update the health bar color
+        if (currentHealth >= (0.5 * MAX_HEALTH))
+            healthBar.UpdateColor(Color.green);
+        else if (currentHealth >= (0.2 * MAX_HEALTH))
+            healthBar.UpdateColor(Color.yellow);
+        else
+            healthBar.UpdateColor(Color.red);
+    }
+
+    /// <summary>
+    /// Sets health to a value between 0 and MAX_HEALTH, inclusive.
+    /// </summary>
+    /// <param name="value">The desired value for the health</param>
+    /// <returns>True if the health successfully changed, false otherwise</returns>
+    public bool setHealth(int value) {
+
+        if (value == currentHealth) return false;
+
+        if (value > MAX_HEALTH
+            || value < 0) return false;
+
+
+
+        currentHealth = value;
+        updateHealthBar();
+
+        return true;
+    }
+
+    /// <summary>
+    /// Adjusts health. Can heal or take damage.
+    /// </summary>
+    /// <param name="delta">Positive for healing, negative for damage</param>
+    /// <returns>True if this function call changed the health, false otherwise</returns>
+    public bool adjustHealth(int delta) {
+
+        bool changed = false;
+
+        // Healing
+        if (delta > 0) {
+            
+            if (currentHealth + delta <= MAX_HEALTH) {
+
+                currentHealth += delta;
+                changed = true;
+            }
+        }
+
+        // Damage
+        else if (delta < 0) {
+            
+            if (currentHealth + delta >= 0) {
+
+                currentHealth += delta;
+                changed = true;
+            }
+        }
+
+        // Avoid doing anything if possible
+        if (!changed) return false;
+
+        updateHealthBar();
+
+        return true;
+    }
+
+    /// <summary>
+    /// Tests whether the object running this script is alive.
+    /// A "dead" object is one whose health has depleted to 0.
+    /// An "alive" object is one whose health is greater than 0.
+    /// </summary>
+    /// <returns>True if alive, false if dead</returns>
+    public bool isNotDead() {
+        return currentHealth > 0;
+    }
+
+    /// <summary>
+    /// Accessor function for this object's current health,
+    /// on a scale from 0 to MAX_HEALTH.
+    /// </summary>
+    /// <returns>This object's current health</returns>
+    public int getCurrentHealth() {
+        return currentHealth;
+    }
+}
