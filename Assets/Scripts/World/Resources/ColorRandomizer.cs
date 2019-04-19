b6de16a90952ffdd18a0434233fb38d7c5460d87
Magnanimous Opicus <mbrad012@ucr.edu>
2019-04-19 02:08:24 +0000

    Michael - Add models to Resources. Add randomized materials to Large and Small Plastic.

---
+++ Assets/Scripts/World/Resources/ColorRandomizer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorRandomizer : RandomMaterialBehavior
+{
+    public List<Color> colors;
+
+    public override void Randomize()
+    {
+        rend.material.color = colors[Random.Range(0, colors.Count)];
+    }
+}
---
+++ Assets/Scripts/World/Resources/TextureRandomizer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureRandomizer : RandomMaterialBehavior
+{
+
+    public List<Texture> textures;
+
+    /// <summary>
+    /// Sets the GameObject's Material to a texture randomly from the given list.
+    /// </summary>
+    public override void Randomize()
+    {
+        rend.material.mainTexture = textures[Random.Range(0, textures.Count)];
+    }
+}
