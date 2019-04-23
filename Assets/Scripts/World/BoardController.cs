a92b98cc989f3b232f71599855aff39ca0c78a99
Magnanimous Opicus <mbrad012@ucr.edu>
2019-04-23 09:14:39 +0000

    Made a tile holder object as part of board, added internal 2D array to hold tiles to board

--- Assets/Scripts/World/BoardController.cs
+++ Assets/Scripts/World/BoardController.cs
@@ -2,57 +2,73 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoardController : MonoBehaviour
-{
-     /// <summary>
-     /// default game tile used for copying and instantiating new tiles
-     /// </summary>
-     public GameObject tile;
-     /// <summary>
-     /// Not currently used, but a 2D array that holds all the tile info.
-     /// Eventually, we'd probably like to associate certain attributes to
-     /// specific tiles, so this is a way to do so.
-     /// </summary>
-     //public GameObject[,] tileArray;
+public class BoardController : MonoBehaviour {
+    /// <summary>
+    /// default game tile used for copying and instantiating new tiles
+    /// </summary>
+    public GameObject emptyTileTemplate;
 
-     /// <summary>
-     /// How many tiles there are in the z direction.
-     /// </summary>
-     private readonly int Z_MAX = 50;
-     /// <summary>
-     /// How many tiles there are in the x direction.
-     /// </summary>
-     private readonly int X_MAX = 50;
+    public GameObject fluffTemplate;
+    public GameObject fiberTemplate;
+    public GameObject objectTemplate;
 
-     // Start is called before the first frame update
-     void Start()
-     {
-          GenerateGrid();
-     }
+    /// <summary>
+    /// Empty GameObject to store all generated tiles in hierarchy
+    /// </summary>
+    public GameObject tileFondler;
 
-     // Update is called once per frame
-     void Update()
-     {
+    private static readonly int TILE_WIDTH = 10;
 
-     }
 
-     /// <summary>
-     /// Called at beginning of game.
-     /// Generates a X_MAX x Z_MAX tile grid out of the tile gameobject
-     /// </summary>
-     private void GenerateGrid()
-     {
-          tile.SetActive(true);
+    /// <summary>
+    /// How many tiles there are in the z direction.
+    /// </summary>
+    private static readonly int Z_MAX = 50;
+    /// <summary>
+    /// How many tiles there are in the x direction.
+    /// </summary>
+    private static readonly int X_MAX = 50;
 
-          for (int x = 0; x < X_MAX; x++)
-          {
-               for (int z = 0; z < Z_MAX; z++)
-               {
-                    var newTile = Instantiate(tile, new Vector3(x * 10, 0, z * 10), Quaternion.identity);
-                    newTile.transform.parent = gameObject.transform;
-               }
-          }
+    /// <summary>
+    /// A 2D array that holds references to all tiles on this board.
+    /// This array is populated within GenerateGrid().
+    /// </summary>
+    public GameObject[,] tileArray = new GameObject[X_MAX, Z_MAX];
 
-          tile.SetActive(false);
-     }
+    // Start is called before the first frame update
+    void Start() {
+        GenerateGrid();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    /// <summary>
+    /// Called at beginning of game.
+    /// Generates a X_MAX x Z_MAX tile grid out of the tile gameobject
+    /// </summary>
+    private void GenerateGrid() {
+
+        emptyTileTemplate.SetActive(true);
+
+        for (int x = 0; x < X_MAX; x++) {
+            for (int z = 0; z < Z_MAX; z++) {
+
+                GameObject newTile = Instantiate(emptyTileTemplate, new Vector3(x * TILE_WIDTH, 0, z * TILE_WIDTH), Quaternion.identity);
+
+                // Store a reference to this tile into a "holder" object, tileFondler
+                newTile.transform.parent = tileFondler.transform;
+
+                // Also store our own reference to this tile in our own 2D array
+                tileArray[x, z] = newTile;
+            }
+        }
+
+        // @TODO: Randomly generate resources and obstacles,
+        // and populate the tileArray here
+
+        emptyTileTemplate.SetActive(false);
+    }
 }
