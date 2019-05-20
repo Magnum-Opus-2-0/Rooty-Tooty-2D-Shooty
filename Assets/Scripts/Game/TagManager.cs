d1e60fd64d2d3a5142dbe9689d5a6514fc9ea0e8
Magnanimous Opicus <mbrad012@ucr.edu>
2019-05-20 05:44:32 +0000

    Ryan - Created Tag Manager script to better handle tag operations. It is ran by GameManager, but feel free to include it in any other GameObject.

---
+++ Assets/Scripts/Game/TagManager.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+/// <summary>
+/// Manager class for different tags.
+/// You must manually edit the code to support new tags.
+/// Go to init() and add your new tag into the corresponding section of code.
+/// Note that if your new tag is shootable,
+/// you will likely need to add your tag to more than 1 area of code.
+/// </summary>
+public class TagManager : MonoBehaviour
+{
+
+    void Awake() {
+
+        init();
+    }
+
+    public bool tagExists(string s) {
+
+        return allTags.Contains(s);
+    }
+
+    public bool isP1Tag(string s) {
+
+        return p1Tags.Contains(s);
+    }
+
+    public bool isP2Tag(string s) {
+
+        return p2Tags.Contains(s);
+    }
+
+    public bool isShootable(string s) {
+
+        return shootables.Contains(s);
+    }
+
+    private void init() {
+
+        // Only populate the static maps once
+        if (allTags != null && allTags.Count > 0) return;
+
+
+
+        allTags = new HashSet<string>();
+        p1Tags = new HashSet<string>();
+        p2Tags = new HashSet<string>();
+        shootables = new HashSet<string>();
+
+        #region Player 1 tags
+        // Manually do Player1 tags
+        // If you need to insert a new p1Tag/p2Tag,
+        // just add it here
+        p1Tags.Add("Player_1");
+        p1Tags.Add("Cam_1");
+        p1Tags.Add("P1canvas");
+        p1Tags.Add("Camera1");
+        p1Tags.Add("P1_Base");
+        p1Tags.Add("Player1_obj");
+        p1Tags.Add("P1_Bullet");
+        p1Tags.Add("P1_Minion");
+        p1Tags.Add("P1_Spawner");
+        p1Tags.Add("P1_Turret");
+        p1Tags.Add("P1_Healer");
+        #endregion
+
+        #region Player 2 tags
+        // Do Player2 tags based on Player 1
+        foreach (string str in p1Tags) {
+
+            string copy = new string(str.ToCharArray());
+            copy.Replace('1', '2');
+
+            Assert.IsTrue(!copy.Contains("1"), "String.Replace() didn't work as intended");
+
+            p2Tags.Add(copy);
+        }
+        Assert.IsTrue(p1Tags.Count == p2Tags.Count);
+        #endregion
+
+        #region Shootables
+        // Do shootables manually
+        shootables.Add("P1_Bullet");
+        shootables.Add("P2_Bullet");
+        shootables.Add("P1_Minion");
+        shootables.Add("P2_Minion");
+        shootables.Add("P1_Spawner");
+        shootables.Add("P2_Spawner");
+        shootables.Add("P1_Turret");
+        shootables.Add("P2_Turret");
+        shootables.Add("P1_Healer");
+        shootables.Add("P2_Healer");
+        #endregion
+
+        #region All tags
+        // Do allTags as a combo of the previous ones,
+        // plus more
+        allTags.UnionWith(p1Tags);
+        allTags.UnionWith(p2Tags);
+        allTags.UnionWith(shootables);
+        allTags.Add("Floor");
+        allTags.Add("Wall");
+        allTags.Add("Resource");
+        allTags.Add("Spawnpoint");
+        allTags.Add("Master Minion Fondler");
+        #endregion
+    }
+
+    private static HashSet<string> allTags;
+    private static HashSet<string> p1Tags;
+    private static HashSet<string> p2Tags;
+    private static HashSet<string> shootables;
+}
