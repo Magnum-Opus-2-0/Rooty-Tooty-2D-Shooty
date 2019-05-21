416648246a80111c8d9ac40d3372e9058d5593b3
Magnanimous Opicus <mbrad012@ucr.edu>
2019-05-21 00:29:52 +0000

    Ross - Got the turrent to not shoot like a drunken idiot. Not sure how. Maybe it just sobered up.

--- Assets/Scripts/World/Buildings/TurretShootController.cs
+++ Assets/Scripts/World/Buildings/TurretShootController.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class TurretShootController : NPCShootController
 {
     [SerializeField]
-    protected Transform barrel_left;
-    [SerializeField]
     protected Transform barrel_right;
+    [SerializeField]
+    protected Transform barrel_left;
+    
 
 
     private bool targetAcquired;
@@ -35,17 +36,17 @@ public class TurretShootController : NPCShootController
 
     public override void Shoot(){
         RecyclableBullet b_f = bulletPool.Request(barrel);
-        RecyclableBullet b_r = bulletPool.Request(barrel_left);
-        RecyclableBullet b_l = bulletPool.Request(barrel_right);
+        RecyclableBullet b_r = bulletPool.Request(barrel_right);
+        RecyclableBullet b_l = bulletPool.Request(barrel_left);
         GameObject go_f = b_f.gameObject;
         GameObject go_r = b_r.gameObject;
         GameObject go_l = b_l.gameObject;
         Rigidbody rb_f = go_f.GetComponent<Rigidbody>();
         Rigidbody rb_r = go_r.GetComponent<Rigidbody>();
         Rigidbody rb_l = go_l.GetComponent<Rigidbody>();
-        rb_f.AddForce(bulletSpeed * barrel.up, ForceMode.VelocityChange);
-        rb_r.AddForce(bulletSpeed * barrel_right.up, ForceMode.VelocityChange);
-        rb_l.AddForce(bulletSpeed * -barrel_left.up, ForceMode.VelocityChange);
+        rb_f.AddForce(bulletSpeed * barrel.forward, ForceMode.VelocityChange);
+        rb_r.AddForce(bulletSpeed * barrel_right.forward, ForceMode.VelocityChange);
+        rb_l.AddForce(bulletSpeed * barrel_left.forward, ForceMode.VelocityChange);
     }
 
 }
