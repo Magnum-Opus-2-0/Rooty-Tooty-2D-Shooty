edf7bfb544e019a0494df770431adb751b38589d
Magnanimous Opicus <mbrad012@ucr.edu>
2019-06-01 23:59:38 +0000

    Michael - Fix Explode Effect Controller issue.

---
+++ Assets/Scripts/Effect Systems/ExplodeEffectController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeEffectController : MonoBehaviour
+{
+    private ParticleSystem ps;
+    private AudioSource noise;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+        noise = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Play the particle system and the audio clip.
+    /// </summary>
+    public void Play()
+    {
+        ps.Play();
+        noise.Play();
+    }
+
+    /// <summary>
+    /// Stop the particle system and the audio clip.
+    /// </summary>
+    public void Stop()
+    {
+        ps.Stop();
+        noise.Stop();
+    }
+}
